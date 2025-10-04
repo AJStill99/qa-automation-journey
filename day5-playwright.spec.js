a5e4a35df04f97dc7907f7f16325eb17a74b8d5d
Alex Still <alexstill@hotmail.co.uk>
2025-10-04 23:09:33 +0100

    Test explanation for day 5

--- day5-playwright.spec.js
+++ day5-playwright.spec.js
@@ -6,3 +6,11 @@ test('visit example.com', async ({ page }) => {
     console.log(`Page title is: ${title}`);
     expect(title).toBe('Wikipedia');
 });
+
+// Test will do:
+// 1. Navigate to example.com
+// 2. Get the page title
+// 3. Log the title to the console
+// 4. Assert the title is "wikipedia"
+
+// To run: npx playwright test day5-playwright.spec.js
