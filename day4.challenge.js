ca04e2b5ef10badd64c8cb2c0e410a6c744827bc
Alex Still <alexstill@hotmail.co.uk>
2025-10-04 14:13:03 +0100

    Day 4 changs - error handling

--- day4.challenge.js
+++ day4.challenge.js
@@ -5,14 +5,17 @@ const users = [
 ];
 
 function checkUserStatus(user) {
+    if (!user.status) {
+    console.log(`No status provided for ${user.username}.`);
+} else {
     if (user.status === "loggedIn") {
-        console.log("Welcome back!");
+        console.log(`Welcome back, ${user.username}!`);
     } else if (user.status === "banned") {
-        console.log("Access Denied. User is banned.");
+        console.log(`Access denied for ${user.username}.`);
     } else {
-        console.log("Please log in.");
+        console.log(`Please log in, ${user.username}.`);
     }
-}
+}};
 
 users.forEach(user => {
     console.log(`Checking status for ${user.username}:`);
