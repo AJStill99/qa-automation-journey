e9e4ca7d648c4d5ccc47ef2c0b4b1008ccb0dffb
Alex Still <alexstill@hotmail.co.uk>
2025-10-28 20:00:14 +0000

    Fixture testing

--- tests/fixture-test.spec.js
+++ tests/fixture-test.spec.js
@@ -5,6 +5,10 @@ test('Checking...', async ({ testFixture }) => {
     
     console.log("Running fixture...")
 
+    // Fixture will run here, as it uses await use() within the fixture itself, so no additional code is needed. 
+
+    console.log("Finished fixture.")
+
     // If we look at the textFixture on ./test-fixtures.js,
     // We have a console.log before and after the await.use() call, which simulates a beforeEach and afterEach
 });
